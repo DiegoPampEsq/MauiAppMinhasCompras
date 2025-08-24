f8cea10baa2d76df15ab960e7402252ea4fe6c52
DiegoPampEsq <diegopamplona32@gmail.com>
2025-08-24 19:26:44 -0300

    Agenda 03

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,6 @@
+using MauiAppMinhasCompras.Models;
+using System.Threading.Tasks;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +9,24 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto produto = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text),
+			};
+
+			await App.Db.Insert(produto);
+			await DisplayAlert("Sucesso!", "Registro Inserido", "Ok");
+
+		} catch (Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "Ok"); 
+		}
+    }
 }
